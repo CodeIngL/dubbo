29820e71da8ac59b4ed643b4d8903b3c9410b829
laihj <laihj@servyou.com.cn>
2017-08-23 13:32:26 +0800

    注释

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/MockInvokersSelector.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/MockInvokersSelector.java
@@ -27,63 +27,91 @@ import com.alibaba.dubbo.rpc.cluster.Router;
 
 /**
  * mock invoker选择器
- * @author chao.liuc
  *
+ * @author chao.liuc
  */
 public class MockInvokersSelector implements Router {
 
-	public <T> List<Invoker<T>> route(final List<Invoker<T>> invokers,
-			URL url, final Invocation invocation) throws RpcException {
-		if (invocation.getAttachments() == null) {
-			return getNormalInvokers(invokers);
-		} else {
-			String value = invocation.getAttachments().get(Constants.INVOCATION_NEED_MOCK);
-			if (value == null) 
-				return getNormalInvokers(invokers);
-			else if (Boolean.TRUE.toString().equalsIgnoreCase(value)){
-				return getMockedInvokers(invokers);
-			} 
-		}
-		return invokers;
-	}
-	
-	private <T> List<Invoker<T>> getMockedInvokers(final List<Invoker<T>> invokers) {
-		if (! hasMockProviders(invokers)){
-			return null;
-		}
-		List<Invoker<T>> sInvokers = new ArrayList<Invoker<T>>(1);
-		for (Invoker<T> invoker : invokers){
-			if (invoker.getUrl().getProtocol().equals(Constants.MOCK_PROTOCOL)){
-				sInvokers.add(invoker);
-			}
-		}
-		return sInvokers;
-	}
-	
-	private <T> List<Invoker<T>> getNormalInvokers(final List<Invoker<T>> invokers){
-		if (! hasMockProviders(invokers)){
-			return invokers;
-		} else {
-			List<Invoker<T>> sInvokers = new ArrayList<Invoker<T>>(invokers.size());
-			for (Invoker<T> invoker : invokers){
-				if (! invoker.getUrl().getProtocol().equals(Constants.MOCK_PROTOCOL)){
-					sInvokers.add(invoker);
-				}
-			}
-			return sInvokers;
-		}
-	}
-	
-	private <T> boolean hasMockProviders(final List<Invoker<T>> invokers){
-		boolean hasMockProvider = false;
-		for (Invoker<T> invoker : invokers){
-			if (invoker.getUrl().getProtocol().equals(Constants.MOCK_PROTOCOL)){
-				hasMockProvider = true;
-				break;
-			}
-		}
-		return hasMockProvider;
-	}
+    /**
+     * @param invokers
+     * @param url        refer url
+     * @param invocation
+     * @param <T>
+     * @return
+     * @throws RpcException
+     */
+    public <T> List<Invoker<T>> route(final List<Invoker<T>> invokers,
+                                      URL url, final Invocation invocation) throws RpcException {
+        if (invocation.getAttachments() == null) {
+            return getNormalInvokers(invokers);
+        } else {
+            String value = invocation.getAttachments().get(Constants.INVOCATION_NEED_MOCK);
+            if (value == null)
+                return getNormalInvokers(invokers);
+            else if (Boolean.TRUE.toString().equalsIgnoreCase(value)) {
+                return getMockedInvokers(invokers);
+            }
+        }
+        return invokers;
+    }
+
+    /**
+     * 返回其中的mockInvoker，
+     * 如果没有返回null
+     *
+     * @param invokers invoker列表
+     * @param <T>
+     * @return mockInvoker列表
+     */
+    private <T> List<Invoker<T>> getMockedInvokers(final List<Invoker<T>> invokers) {
+        if (!hasMockProviders(invokers)) {
+            return null;
+        }
+        List<Invoker<T>> sInvokers = new ArrayList<Invoker<T>>(1);
+        for (Invoker<T> invoker : invokers) {
+            if (Constants.MOCK_PROTOCOL.equals(invoker.getUrl().getProtocol())) {
+                sInvokers.add(invoker);
+            }
+        }
+        return sInvokers;
+    }
+
+    /**
+     * 返回非mockinvoker
+     *
+     * @param invokers invoker列表
+     * @param <T>
+     * @return 非mockinvoker（普通的invoker)
+     */
+    private <T> List<Invoker<T>> getNormalInvokers(final List<Invoker<T>> invokers) {
+        if (!hasMockProviders(invokers)) {
+            return invokers;
+        } else {
+            List<Invoker<T>> sInvokers = new ArrayList<Invoker<T>>(invokers.size());
+            for (Invoker<T> invoker : invokers) {
+                if (!Constants.MOCK_PROTOCOL.equals(invoker.getUrl().getProtocol())) {
+                    sInvokers.add(invoker);
+                }
+            }
+            return sInvokers;
+        }
+    }
+
+    /**
+     * 检测是否有入参中是否有mock的Invoker
+     *
+     * @param invokers invoker列表
+     * @param <T>
+     * @return 是否含有mock的Invoker
+     */
+    private <T> boolean hasMockProviders(final List<Invoker<T>> invokers) {
+        for (Invoker<T> invoker : invokers) {
+            if (Constants.MOCK_PROTOCOL.equals(invoker.getUrl().getProtocol())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public URL getUrl() {
         return null;
