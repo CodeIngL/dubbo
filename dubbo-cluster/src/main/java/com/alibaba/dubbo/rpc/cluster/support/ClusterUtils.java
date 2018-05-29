26eeacc1d88fa84f553aa0f2024ed0d38984d6d3
laihj <laihj@servyou.com.cn>
2018-05-29 10:09:34 +0800

    use utils

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/ClusterUtils.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/ClusterUtils.java
@@ -18,9 +18,12 @@ package com.alibaba.dubbo.rpc.cluster.support;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.utils.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.alibaba.dubbo.common.utils.MapUtils.isNotEmpty;
 
 /**
  * ClusterUtils
@@ -36,7 +39,7 @@ public class ClusterUtils {
         Map<String, String> remoteMap = remoteUrl.getParameters();
 
 
-        if (remoteMap != null && remoteMap.size() > 0) {
+        if (isNotEmpty(remoteMap)) {
             map.putAll(remoteMap);
 
             // Remove configurations from provider, some items should be affected by provider.
@@ -62,43 +65,41 @@ public class ClusterUtils {
             map.remove(Constants.DEFAULT_KEY_PREFIX + Constants.TRANSPORTER_KEY);
         }
 
-        if (localMap != null && localMap.size() > 0) {
+        if (isNotEmpty(localMap)) {
             map.putAll(localMap);
         }
-        if (remoteMap != null && remoteMap.size() > 0) {
+        if (isNotEmpty(remoteMap)) {
             // Use version passed from provider side
             String dubbo = remoteMap.get(Constants.DUBBO_VERSION_KEY);
-            if (dubbo != null && dubbo.length() > 0) {
+            if (StringUtils.isNotEmpty(dubbo)) {
                 map.put(Constants.DUBBO_VERSION_KEY, dubbo);
             }
             String version = remoteMap.get(Constants.VERSION_KEY);
-            if (version != null && version.length() > 0) {
+            if (StringUtils.isNotEmpty(version)) {
                 map.put(Constants.VERSION_KEY, version);
             }
             String group = remoteMap.get(Constants.GROUP_KEY);
-            if (group != null && group.length() > 0) {
+            if (StringUtils.isNotEmpty(group)) {
                 map.put(Constants.GROUP_KEY, group);
             }
             String methods = remoteMap.get(Constants.METHODS_KEY);
-            if (methods != null && methods.length() > 0) {
+            if (StringUtils.isNotEmpty(methods)) {
                 map.put(Constants.METHODS_KEY, methods);
             }
             // Reserve timestamp of provider url.
             String remoteTimestamp = remoteMap.get(Constants.TIMESTAMP_KEY);
-            if (remoteTimestamp != null && remoteTimestamp.length() > 0) {
+            if (StringUtils.isNotEmpty(remoteTimestamp)) {
                 map.put(Constants.REMOTE_TIMESTAMP_KEY, remoteMap.get(Constants.TIMESTAMP_KEY));
             }
             // Combine filters and listeners on Provider and Consumer
             String remoteFilter = remoteMap.get(Constants.REFERENCE_FILTER_KEY);
             String localFilter = localMap.get(Constants.REFERENCE_FILTER_KEY);
-            if (remoteFilter != null && remoteFilter.length() > 0
-                    && localFilter != null && localFilter.length() > 0) {
+            if (StringUtils.isNotEmpty(remoteFilter) && StringUtils.isNotEmpty(localFilter)) {
                 localMap.put(Constants.REFERENCE_FILTER_KEY, remoteFilter + "," + localFilter);
             }
             String remoteListener = remoteMap.get(Constants.INVOKER_LISTENER_KEY);
             String localListener = localMap.get(Constants.INVOKER_LISTENER_KEY);
-            if (remoteListener != null && remoteListener.length() > 0
-                    && localListener != null && localListener.length() > 0) {
+            if (StringUtils.isNotEmpty(remoteListener) && StringUtils.isNotEmpty(localListener)) {
                 localMap.put(Constants.INVOKER_LISTENER_KEY, remoteListener + "," + localListener);
             }
         }
